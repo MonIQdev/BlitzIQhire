4b48b553f5a41689aaf380f6e56667d409dda02e
MonIQdev <MonIQdev1@gmail.com>
2026-08-23 14:03:31 -0400

    chore: add next.config.js to enable webpack alias resolution on Vercel

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@/components': path.resolve(__dirname, 'components'),
+      '@/lib': path.resolve(__dirname, 'lib'),
+      '@/app': path.resolve(__dirname, 'app'),
+      '@/styles': path.resolve(__dirname, 'styles'),
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
